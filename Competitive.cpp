b6429443055b89cd8a65d07b8b1e4ce4f8f10fa0
Chaitya Shah <Chaitya62@gmail.com>
2016-09-23 01:46:22 +0530

    cpp template added

---
+++ Competitive.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<algorithm>
+#include<map>
+#include<set>
+using namespace std;
+#define uii long long int
+#define M(a,b) (a>b ? a : b)
+#define m(a,b) (a>b ? b : a)
+#define it(a)  ::iterator it
+#define slld(a) scanf("%lld",&a)
+#define plld(a) printf("%lld",a)
+
+int main(){
+	
+
+
+
+
+
+	return 0;
+}
+
